1139752d8f01bce3dc720f1302545594e2c1bb43
Alexander Glushkov <cutalion@gmail.com>
2013-04-04 19:30:30 +0300

    Random client

--- server/node_info.js
+++ server/node_info.js
@@ -5,12 +5,29 @@ module.exports = function(name) {
   self.name   = name;
   self.x      = null;
   self.y      = null;
+  self.old    = 
   self.energy = null;
   self.score  = 0;
 
   // move that is valid for the upcoming tick
   // cleared on every tick.
   self.tickMove = null;
+
+  self.move_left = function(){
+    self.x--;
+  }
+
+  self.move_down = function(){
+    self.y++;
+  }
+
+  self.move_up = function(){
+    self.y--;
+  }
+
+  self.move_right = function(){
+    self.x++;
+  }
 
   return self;
 }
